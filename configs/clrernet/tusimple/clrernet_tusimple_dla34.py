485ecd3f1889dd0a8bd63692e887d8cb5c362d51
seasoning <bw_chen1223@foxmail.com>
2024-08-06 10:36:32 +0800

    config: Adjust the width when calculating losses on the tusimle dataset

--- configs/clrernet/tusimple/clrernet_tusimple_dla34.py
+++ configs/clrernet/tusimple/clrernet_tusimple_dla34.py
@@ -36,7 +36,7 @@ model = dict(
         loss_bbox=dict(type="SmoothL1Loss", reduction="none", loss_weight=0.2),
         loss_iou=dict(
             type="LaneIoULoss",
-            lane_width=2.5 / 800,
+            # lane_width=2.5 / 800,
             loss_weight=4.0,
         ),
         loss_seg=dict(
@@ -49,13 +49,13 @@ model = dict(
         assigner=dict(
             iou_dynamick=dict(
                 type="LaneIoUCost",
-                lane_width=2.5 / 800,
+                # lane_width=2.5 / 800,
                 use_pred_start_end=False,
                 use_giou=True,
             ),
             iou_cost=dict(
                 type="LaneIoUCost",
-                lane_width=10 / 800,
+                # lane_width=10 / 800,
                 use_pred_start_end=True,
                 use_giou=True,
             ),
